0315b0b6dda69f83e3f22d4ee3a83876c2ef1443
girishrd <girish.rd@gmail.com>
2011-03-07 15:33:42 -0800

    Edited minterm.h via GitHub

--- minterm.h
+++ minterm.h
@@ -1,10 +1,12 @@
-
 class minterm
 {
 private:
-string term;   //The minterm specified eg"1001"
-int onesCount; //a count of the number of ones in the minterm      
-vector<int> xpos; //the bit positions of 'x' in this minterm.
-minterm(string)
+vector<short> minterm;
+
 ~minterm();
+
+string canCombine(minterm &m); 
+int countParameters(parameter);
+compareMinterms()
+
 };
