63758229b2c090c6d848714c9f51699752669c2d
girishrd <girish.rd@gmail.com>
2011-03-08 09:33:20 -0800

    Edited minterm.cc via GitHub

--- minterm.cc
+++ minterm.cc
@@ -1,55 +1,113 @@
-/*
-Filename: minterm.cc
-description:class definition for minterm
-created:6-3-2011
+/*Filename: main.cpp
+desc:main method for minlogic
 */
-
 #include <minterm.h>
-#include<string>
-#include<vector>
+#include <cstdlib>
+#include <iostream>
+#include <string>
+#include <list>
+#include <map>
+#define DONTCARE "d"
 
-//constructor 
-minterm::minterm(const short mterm)
+using namespace std;
+//global list of prime implicants
+list<minterm> primeImplicants;
+
+int main(int argc, char *argv[])
 {
-//fill up all the 'details'
-//minterm value
-mtermVal.push_back(mterm);
+	if(argc != 2)
+	{
+		cout << "Usage: " << argv[0] << "filename" << endl;
+		exit(1);
+	}
+//check if the input minterm file exists
+		ifstream fin(argv[1]);
+
+	if(!fin)
+	{
+		cout << "Minterm (input) file " << argv[1] << " not found." << endl;
+		exit(1);
+	}
+string fileData;        //used to read the implicant input file    
+string buffer;
+list<minterm>dontCares; //list of all don't cares
+list<minterm>allMinterms; //list of all minterms (this includes dc)
+int noOfvars; 
+int noOfTerms;
+   
+//read the no.of vars and no of terms
+getline(fin,fileData);
+noOfVars= atoi(fileData.c_str());
+getline(fin,fileData);
+noOfTerms=atoi(fileData.c_str());
+size_t found;
+//read all minterms from file and add to appropriate lists
+while( mintermcount< noOfTerms)
+{
+         getline(fin,fileData);
+         //If a term is marked as dont care ,put it into the dc and mintern lists.
+          size_t found=fileData.find(DONTCARE);
+          if (found!=string::npos) // the term is a dont care eg: 1001 d
+          {
+             fileData.copy(buffer,noOfVars);
+             try  //NOTE to self: looks weird but might work ref http://www.codeguru.com/forum/showthread.php?t=231054
+             {
+                minterm m(lexical_cast<short>(buffer.c_str()) )
+                dontCares.push_back(m);
+                allMinterms.push_back(m);
+             }
+             catch(bad_lexical_cast&)
+             {
+                dontCares.push_back(0);
+                allMinterms.push_back(0));
+             }
+            
+          }
+        else //the term is not a dc - so add it only to the minterm list
+         {
+                         
+           fileData.copy(buffer,noOfVars);
+             try  //NOTE to self: looks weird but might work ref http://www.codeguru.com/forum/showthread.php?t=231054
+             {
+                minterm m(lexical_cast<short>(buffer.c_str()) )
+                allMinterms.push_back(m);
+             }
+             catch(bad_lexical_cast&)
+             {
+                allMinterms.push_back(0));
+             }
+         }
+         mintermcount++;        
+             
 }
-minterm::~minterm(void)
 
+vector< vector <vector<minterm> > mintermArray();
 
-//compares the objs mintermVal with parameters and returns the 
-//combined value else returns NULL.
-minterm minterm::canCombine(minterm& other)
+//initial setup: fill up all the uncombined terms ( all terms will end up in the first row -with zero 'x's)
+for(i=0; i < mintermArray.size() ; ++i)
+for ( j=0; j < mintermArray[i].size();j++)
 {
- vector<short>::iterator it;
- vector<short>::iterator otherit;
- //do a bit wise  minterm comparison and insert 'x' whereever they differ.
- for (it=this->mintermVal.begin() , otherit=other.begin(); it < this->mtermVal.end(); it++ ,otherit++)
- {   
-     if ( *it != *otherit)
-     {
-      mintermVal.insert(it,'x',1);  //VERIFY:is this a valid expression?
-      bitDiff++;
-     }
- }
- //return the result ONLY if there is a one bit difference else ret NULL
- if( bitDiff==1)
- return(mintermVal);
- else
- return NULL;
-}
+    mintermit=allMinterms.begin();
+       for ( mintermit=allMinterms.begin() ; mintermit != allMinterms.end(); mintermit++ )
+           if (mintermit->countParameters(1)==j) //check for the no of ones only (since we know no of xs is zero anyway)
+           mintermArray[i][j]=(*mintermit);  //VERIFY:is this syntax right?
+    }
 
-
-//counts the number of 'p's in the minterm vector and returns the count
-int minterm::countParam(short p)
-{
- int pcount =0;
- vector<short>::iterator it;
- for (it=this->mintermVal.begin() ; it < this->mtermVal.end(); it++ )
+//QM :combining terms 
+//run thru the array from top to bottom and check adjacent cells i,j if they can be combined
+//put the result in the [i+1th][j-1th] location
+for(i=0; i < mintermArray.size() ; ++i)
+for ( j=0; j < mintermArray[i].size();j++)
  {
-     if( *it=p) 
-     pcount++;
+       ( minterm[i][j].canCombine(minterm[i][j]) )
+    {//adjacent terms differ by one bit so combine
+      
+    }
+    
+       
  }
- return(pcount); 
-}
+ 
+
+
+
+
