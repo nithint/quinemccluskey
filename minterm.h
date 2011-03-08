72634c5438f93818677ebaa25baa628c69026c3a
girishrd <girish.rd@gmail.com>
2011-03-08 09:23:27 -0800

    Edited minterm.h via GitHub

--- minterm.h
+++ minterm.h
@@ -1,12 +1,22 @@
+#include <vector>
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+using std::pair;
+using std::make_pair;
+
 class minterm
 {
 private:
-vector<short> minterm;
+vector<short>mintermVal; // bits of 'this' minterm eg:0001
+//vector<int> xpos;        //the bit positions of 'x' in this minterm.
 
+public:
+minterm(short);
 ~minterm();
 
-string canCombine(minterm &m); 
-int countParameters(parameter);
-compareMinterms()
-
+minterm canCombine(&minterm); //compares the objs mintermVal with parameters and returns the 
+                              //combined value else returns NULL.
+int countParameters(short); // returns the no.of parameters present in the minterm
 };
