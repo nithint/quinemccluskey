f7f576f804247560f7c3c45c35daffd84515a45b
unknown <girish@.(none)>
2011-03-08 11:18:00 -0600

    added cancombine method

--- minterm.h
+++ minterm.h
@@ -1,10 +1,34 @@
+/*
+ * minterm.h
+ *
+ *  Created on: Jan 23, 2011
+ */
+#include <string>
+#include <map>
+/*
+Filename: minterm.h
+description:header for minterm class
+created:6-3-2011
+*/
+#include <vector>
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+using std::pair;
+using std::make_pair;
 
 class minterm
 {
 private:
-string term;   //The minterm specified eg"1001"
-int onesCount; //a count of the number of ones in the minterm      
-vector<int> xpos; //the bit positions of 'x' in this minterm.
-minterm(string)
+vector<short>mintermVal; // bits of 'this' minterm eg:0001
+//vector<int> xpos;        //the bit positions of 'x' in this minterm.
+
+public:
+minterm(short);
 ~minterm();
+
+minterm canCombine(&minterm); //compares the objs mintermVal with parameters and returns the 
+                              //combined value else returns NULL.
+int countParameters(short); // returns the no.of parameters present in the minterm
 };
